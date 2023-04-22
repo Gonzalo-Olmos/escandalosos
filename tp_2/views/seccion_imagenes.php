42c0a8dd0784ad4c1d9edf5068bd4ffbdfa91c50
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-04-21 22:32:36 -0300

    avances

---
+++ tp_2/views/seccion_imagenes.php
@@ -0,0 +1,238 @@
+
+<!-- Fila[3] Desktop -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(0)">
+          <img src="../assets/img/INFPROM_1_CAT_0.png" class="card-img-top" alt="Infantil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Promocional</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(1)">
+          <img src="../assets/img/INFELIT_CAT_1.png" class="card-img-top" alt="Infantil Elite">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Elite</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(2)">
+          <img src="../assets/img/CADEPROM_CAT_2.png" class="card-img-top" alt="Cadete Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Cadete Promocional</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(3)">
+          <img src="../assets/img/CadeteElitePortada.png" class="card-img-top" alt="Cadete Elite">
+          <div class="card-body">
+            <h5 class="card-title">Cadete Elite</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(4)">
+          <img src="../assets/img/juvenilPromPort.png" class="card-img-top" alt="Juvenil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Juvenil Promocional</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(5)">
+          <img src="../assets/img/JuvenilElitePortada.png" class="card-img-top" alt="Juvenil Elite">
+          <div class="card-body">
+            <h5 class="card-title">Juvenil Elite</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(6)">
+          <img src="../assets/img/ADULPROMENOS_CAT_6.png" class="card-img-top" alt="Adulto Promocional menor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Promocional Menor a 30</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(7)">
+          <img src="../assets/img/adultoEliteMenos.png" class="card-img-top" alt="Adulto Elite menor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Menor a 30</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(8)">
+          <img src="../assets/img/ADULPROMAS_CAT_8.png" class="card-img-top" alt="Adulto Promocional mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Promocional Mayor a 30</h5>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(9)">
+          <img src="../assets/img/adultoElitMas.png" class="card-img-top" alt="Adulto Elite mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Mayor a 30</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(0)">
+          <img src="../assets/img/InfPromCarru.png" class="card-img-top" alt="Infantil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Promocional</h5>
+          </div>
+        </div>
+        <div class="card" onclick="desplegarContenido(9)">
+          <img src="../assets/img/ADULELITMAS_2_CAT_9.png" class="card-img-top" alt="Adulto Elite mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Mayor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<!-- Fila[3] Mobile -->
+<div id="carouselExampleControlsMobile" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(0)">
+          <img src="../assets/img/INFPROM_1_CAT_0.png" class="card-img-top" alt="Infantil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Promocional</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(1)">
+          <img src="../assets/img/INFELIT_CAT_1.png" class="card-img-top" alt="Infantil Elite">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Elite</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(2)">
+          <img src="../assets/img/CADEPROM_CAT_2.png" class="card-img-top" alt="Cadete Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Cadete Promocional</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(3)">
+          <img src="../assets/img/CADELIT_CAT_3.png" class="card-img-top" alt="Cadete Elite">
+          <div class="card-body">
+            <h5 class="card-title">Cadete Elite</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(4)">
+          <img src="../assets/img/JUVEPROM_CAT_4.png" class="card-img-top" alt="Juvenil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Juvenil Promocional</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(5)">
+          <img src="../assets/img/JUVELIT_CAT_5.png" class="card-img-top" alt="Juvenil Elite">
+          <div class="card-body">
+            <h5 class="card-title">Juvenil Elite</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(6)">
+          <img src="../assets/img/ADULPROMENOS_CAT_6.png" class="card-img-top" alt="Adulto Promocional menor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Promocional Menor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(7)">
+          <img src="../assets/img/ADULELITMENOS_CAT_7.png" class="card-img-top" alt="Adulto Elite menor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Menor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(8)">
+          <img src="../assets/img/ADULPROMAS_CAT_8.png" class="card-img-top" alt="Adulto Promocional mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Promocional Mayor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(9)">
+          <img src="../assets/img/ADULELITMAS_CAT_9.png" class="card-img-top" alt="Adulto Elite mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Mayor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(9)">
+          <img src="../assets/img/ADULELITMAS_2_CAT_9.png" class="card-img-top" alt="Adulto Elite mayor a 30">
+          <div class="card-body">
+            <h5 class="card-title">Adulto Elite Mayor a 30</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="cards-wrapper">
+        <div class="card" onclick="desplegarContenido(0)">
+          <img src="../assets/img/INFPROM_2_CAT_0.png" class="card-img-top" alt="Infantil Promocional">
+          <div class="card-body">
+            <h5 class="card-title">Infantil Promocional</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControlsMobile" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControlsMobile" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
