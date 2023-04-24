e8f836ee281f5334df385e879ee874b21be3c888
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-04-24 15:27:20 -0300

    fix mejoras

--- tp_2/views/seccion_5.php
+++ tp_2/views/seccion_5.php
@@ -1,20 +1,20 @@
 <div class="row ">
     <div class="table-responsive">
         <table id="competidores_tabla"
-            class="table hover table-bordered nowrap no-footer table-striped table-condensed flip-content">
+            class="table hover table-bordered nowrap border">
             <thead class="flip-content">
                 <tr>
                     <th data-priority="1"> GAL </th>
                     <th data-priority="1"> Apellido </th>
                     <th data-priority="1"> Nombre </th>
                     <th data-priority="1"> DU </th>
-                    <th data-priority="1"> Fecha de Nacimiento </th>
-                    <th data-priority="1"> Pais | Estado </th>
-                    <th data-priority="1"> Graduacion</th>
-                    <th data-priority="1"> Clasificacion general </th>
-                    <th data-priority="1"> Email </th>
-                    <th data-priority="1"> Genero </th>
-                    <th data-priority="1" width="3%"> Acciónes </th>
+                    <th data-priority="3"> Fecha de Nacimiento </th>
+                    <th data-priority="3"> Pais | Estado </th>
+                    <th data-priority="3"> Graduacion</th>
+                    <th data-priority="3"> Clasificacion general </th>
+                    <th data-priority="3"> Email </th>
+                    <th data-priority="3"> Genero </th>
+                    <th data-priority="3"> Acciónes </th>
                 </tr>
             </thead>
             <tbody>
