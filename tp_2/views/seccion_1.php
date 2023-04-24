ee161a71165581665b7195a5bde4d0a082c289a3
GustavoOliveros <gusa05+1@gmail.com>
2023-04-24 00:31:43 -0300

    Terminado sección 1 y 2

--- tp_2/views/seccion_1.php
+++ tp_2/views/seccion_1.php
@@ -8,3 +8,22 @@ momento, no hace falta que llegue a OVERTIME
 Al presionar el botón Termina además deja visible el tiempo transcurrido, el cartel de
 OVERTIME y agrega a la vista un valor con la cantidad de tiempo total
 </p>
+
+<div class="row text-center">
+    <div class="col-6">
+        <h3 id="contador">90 seg.</h3>
+    </div>
+</div>
+<div class="row text-center">
+    <div class="col-3">
+        <button class="btn btn-primary" id="inicio-contador">Inicia</button>
+    </div>
+    <div class="col-3">
+        <button class="btn btn-secondary disabled" id="fin-contador">Termina</button>
+    </div>
+</div>
+<div class="row text-center">
+    <div class="col-6">
+        <p id="tiempo-total"></p>
+    </div>
+</div>
--- tp_2/views/seccion_2.php
+++ tp_2/views/seccion_2.php
@@ -6,3 +6,15 @@ Esto cuidaría la descarga innecesaria de datos.
 a. Si la vista es mobile hacer 1 link.
 b. Si es la vista desktop mostrar el video.
 </p>
+
+<div class="row d-block d-md-none">
+    <div class="col-12">
+        <a href="https://www.youtube.com/watch?v=ermNRSmkGF8" target="_blank" class="btn btn-primary">Haga clic acá para ver un vídeo</a>
+    </div>
+</div>
+
+<div class="row d-none d-md-block">
+    <div class="col-12">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ermNRSmkGF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+</div>
