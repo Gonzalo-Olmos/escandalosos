0e6d925b70455630ab845a69fecdd33bdda8dbbe
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-04-24 16:26:47 -0300

    fis comentarios utiles

--- tp_2/views/index.php
+++ tp_2/views/index.php
@@ -1,8 +1,10 @@
 <?php include_once("common/header.php"); ?>
 
+<!--Consigna de tp  2.1 -->
 <div class="row mx-md-3 mx-auto" id="competidores" style="margin-top: 100px;">
 </div>
 
+<!--Consigna de tp  2.2 -->
 <div class="row mx-md-3">
   <h1> Seccion 1</h1>
   <?php include_once("./seccion_1.php"); ?>
@@ -19,16 +21,20 @@
     <?php include_once("./seccion_3.php"); ?>
 </div>
 
+
+<!--Consigna convinada de tp  2.3 y 2.1-->
 <div class="row mx-md-3">
   <h1> Formulario de Competidor </h1>
     <?php include_once("./seccion_4.php"); ?>
 </div>
 
+<!--Consigna convinada de tp  2.3-->
 <div class="row mx-md-3">
   <h1> Competidores </h1>
     <?php include_once("./seccion_5.php"); ?>
 </div>
 
+<!--Consigna de tp  2.3 -->
 <div class="row mx-md-3">
   <h1> Imagenes </h1>
     <?php include_once("./seccion_6.php"); ?>
