a0c770647451dc98552f02f08cb1b5bb6ae36961
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-04-29 17:01:37 -0300

    avances en temas

--- tp_2/assets/js/cambios_de_tema.js
+++ tp_2/assets/js/cambios_de_tema.js
@@ -1,17 +1,26 @@
 
 export function cambio_de_tema(){
+    var tabla = document.querySelector('.table-light');
+    tabla = document.querySelector('.table-dark');
+
     if (localStorage.getItem("background") === "dark" || localStorage.getItem("background") === null) {
-        document.querySelector('body').setAttribute('data-bs-theme','dark')
+        document.querySelector('body').setAttribute('data-bs-theme','dark');
+        tabla.classList['value'] = 'table hover  table-dark table-bordered nowrap border';
       } else if (localStorage.getItem("background") === "light") {
-        document.querySelector('body').setAttribute('data-bs-theme','light')
+        document.querySelector('body').setAttribute('data-bs-theme','light');
+        tabla.classList['value'] = 'table hover table-light table-bordered nowrap border';
+    
       }
     
       document.getElementById('cambiarVista').addEventListener("click", function() {
         if(localStorage.getItem("background") === "dark"){
-          document.querySelector('body').setAttribute('data-bs-theme','dark')
+          document.querySelector('body').setAttribute('data-bs-theme','dark');
+          tabla.classList['value'] = 'table hover  table-dark table-bordered nowrap border';
           localStorage.setItem("background", "light");
         }else{
-          document.querySelector('body').setAttribute('data-bs-theme','light')
+          document.querySelector('body').setAttribute('data-bs-theme','light');
+          tabla.classList['value'] = 'table hover table-light table-bordered nowrap border';
+    
           localStorage.setItem("background", "dark");
         }
       });
@@ -21,4 +30,12 @@ export function cambio_de_tema(){
     
       }
 
+}
+
+function cambiar_estilo_tabla_dark(){
+    var tabla = document.querySelector('.table-light');
+
+    tabla.classList['value'] = 'table hover table-light table-bordered nowrap border';
+    tabla.classList['value'] = 'table hover  table-dark table-bordered nowrap border';
+  
 }
